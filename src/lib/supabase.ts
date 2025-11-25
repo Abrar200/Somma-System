1b080788d30262ab72f7b23a5679b61da742a4d6
Abrar200 <abrarshahriar360@gmail.com>
2025-11-25 13:03:02 +1030

    first commit

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 
 // Initialize Supabase client
 // Using direct values from project configuration
-const supabaseUrl = 'https://zoljzcdwthnahapjxtdl.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvbGp6Y2R3dGhuYWhhcGp4dGRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQxMTc1OTcsImV4cCI6MjA2OTY5MzU5N30.xQjGcYS8oc6sgOJWh01P03Vwaca1Tp-mMMyCq2iSU8k';
+const supabaseUrl = 'https://kxujxtpabiclsnefwohk.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4dWp4dHBhYmljbHNuZWZ3b2hrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMjc1ODYsImV4cCI6MjA3OTYwMzU4Nn0.n2lq8PkELPOUic5Njf88DqHcY06-3FzdA4dwaiXiGDU';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 
